396399b3aacb010aff4630471cb38bdda48c8603
maia <matteusmaia889@gmail.com>
2023-11-06 13:33:50 -0300

    Adicionando o range

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
+let passwordLength = 16
+
+
 function generatePassword(){
     const chars = "abcdefghjklmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYS123456789?!@&*()[]"
     let password = ""
 
-    for(let i = 0; i < 8; i++){
+    for(let i = 0; i < passwordLength; i++){
         const randomNumber = Math.floor(Math.random() * chars.length)
         password += chars.substring(randomNumber, randomNumber + 1)
 
     }
     const inputEl = document.querySelector("#password") 
-    inputEl.value = password
+    inputEl.value = password 
 }
+
+const passwordLengthEl = document.querySelector("#password-length")
+passwordLengthEl.addEventListener("input", function() {
+    passwordLength = passwordLengthEl.value
+    generatePassword()
+})
 
 generatePassword()
