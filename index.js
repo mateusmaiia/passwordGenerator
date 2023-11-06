5a5edaa5389e2f6c9d5f8912292cba9fac52d172
maia <matteusmaia889@gmail.com>
2023-11-06 13:24:43 -0300

    input password

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 function generatePassword(){
-    const chars = "abcdefghjklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYS123456789?!@&*()[]"
+    const chars = "abcdefghjklmnpqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYS123456789?!@&*()[]"
     let password = ""
 
     for(let i = 0; i < 8; i++){
         const randomNumber = Math.floor(Math.random() * chars.length)
         password += chars.substring(randomNumber, randomNumber + 1)
-    }
 
-    generatePassword()
+    }
+    const inputEl = document.querySelector("#password") 
+    inputEl.value = password
 }
+
+generatePassword()
